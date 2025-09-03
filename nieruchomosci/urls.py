be97a33158687663d9be923983d804692b2e3712
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-03 22:04:37 +0200

    fix

--- nieruchomosci/urls.py
+++ nieruchomosci/urls.py
@@ -19,12 +19,20 @@ from django.urls import path, include
 from django.conf import settings
 from oferty import views
 from django.conf.urls.static import static
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('oferty.urls')),  
+    path('', include('nieruchomosci.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='api-schema'), name='api-redoc'),
 ]
 
 
