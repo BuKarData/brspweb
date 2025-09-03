66a985222d81b07f09f5e145fa9a733b56b41455
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-03 21:10:23 +0200

    fix

--- oferty/urls.py
+++ oferty/urls.py
@@ -4,17 +4,8 @@ from oferty import views
 from django.conf import settings
 from django.conf.urls.static import static
 from .api_views import InwestycjaListAPIView, OfertaListAPIView, CenaListAPIView, APISchemaView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.renderers import JSONOpenAPIRenderer
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('oferty/', views.lista_ofert, name='lista_ofert'),
-    path('api/inwestycje/', InwestycjaListAPIView.as_view(), name='api-inwestycje'),
-    path('api/oferty/', OfertaListAPIView.as_view(), name='api-oferty'),
-    path('api/ceny/', CenaListAPIView.as_view(), name='api-ceny'),
-    path('api/schema/', APISchemaView.as_view(), name='api-schema')
-]
 
 
 class SpectacularInlineAPIView(SpectacularAPIView):
@@ -24,6 +15,17 @@ class SpectacularInlineAPIView(SpectacularAPIView):
         response = super().get(request,*args,**kwargs)
         response['Content-Disposition'] = 'inline' 
         return response
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('oferty/', views.lista_ofert, name='lista_ofert'),
+    path('api/inwestycje/', InwestycjaListAPIView.as_view(), name='api-inwestycje'),
+    path('api/oferty/', OfertaListAPIView.as_view(), name='api-oferty'),
+    path('api/ceny/', CenaListAPIView.as_view(), name='api-ceny'),
+    path('api/schema/', SpectacularInlineAPIView.as_view(), name='api-schema')
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
