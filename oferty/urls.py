61406f99a7fb3fae22924244c7c8be9c6c4bdfa9
Karol Budzyn <kaponers@Raccoons-MacBook.local>
2025-09-03 18:47:13 +0200

    fix

--- oferty/urls.py
+++ oferty/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from oferty import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .api_views import OfertyAPIView
 
 urlpatterns = [
     path('', views.home, name='home'),
